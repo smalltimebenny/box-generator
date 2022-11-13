d8d5563ffa389c074dd05a69c69df74113c04365
smalltimebenny <ben.lindquist@gmail.com>
2022-11-13 00:28:54 -0500

    add comment on unused BoxDisplay component

--- src/components/BoxDisplay.jsx
+++ src/components/BoxDisplay.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 
+// i spent a long time messing around with this when i could have just done it all on one pageXOffset. i assumed we would need to lift state. if we were supposed to and i missed it, i would liek to know what i did wrong.
+
 const BoxDisplay = (props) => {
     console.log("this is backgorund color", props.backgroundColor)
     const boxStyle = {
